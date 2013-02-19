34fede59559219cbb8b609e709cd4ba24c15eaa2
Christoffer Niska <ChristofferNiska@gmail.com>
2013-02-19 02:16:57 +0200

    Rename configuration files

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 $yii = __DIR__ . '/../app/vendor/yiisoft/yii/framework/yii.php';
 $globals = __DIR__ . '/../app/globals.php';
-$config = __DIR__ . '/../app/config/prod.php';
+$config = __DIR__ . '/../app/config/main.php';
 
 require_once($yii);
 require_once($globals);
