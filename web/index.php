2491de7e972a086e10c6bafdb1a64b5b392bfa9b
Christoffer Niska <ChristofferNiska@gmail.com>
2013-03-28 00:32:27 +0200

    Update entry script

--- web/index.php
+++ web/index.php
@@ -8,13 +8,13 @@ Debugger::init(__DIR__ . '/../app/runtime/debug');
 
 $yii = __DIR__ . '/../vendor/yiisoft/yii/framework/yii.php';
 $global = __DIR__ . '/../app/helpers/global.php';
-$builder = __DIR__ . '/../vendor/crisu83/yii-configbuilder/helpers/EnvConfigBuilder.php';
+$builder = __DIR__ . '/../vendor/crisu83/yii-configbuilder/helpers/ConfigBuilder.php';
 
 require_once($yii);
 require_once($global);
 require_once($builder);
 
-$config = EnvConfigBuilder::build(array(
+$config = ConfigBuilder::buildForEnv(array(
 	__DIR__ . '/../app/config/main.php',
 	__DIR__ . '/../app/config/web.php',
 	__DIR__ . '/../app/config/environments/{environment}.php',
