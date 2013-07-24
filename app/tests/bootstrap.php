ee226133742b44e7ae9c9f9b58dab4cb2f9a6cfc
Christoffer Niska <ChristofferNiska@gmail.com>
2013-07-24 17:18:33 +0300

    Update bootstrap.php

--- app/tests/bootstrap.php
+++ app/tests/bootstrap.php
@@ -1,10 +1,8 @@
 <?php
 
-// change the following paths if necessary
-$yiit = dirname(__FILE__) . '/../../yii/yiit.php';
-$config = dirname(__FILE__) . '/../config/test.php';
+$config = __DIR__ . '/../config/test.php';
 
-require_once($yiit);
-require_once(dirname(__FILE__) . '/WebTestCase.php');
+require(__DIR__ . '/../app/vendor/yiisoft/yii/framework/yiit.php');
+require(__DIR__ . '/WebTestCase.php');
 
 Yii::createWebApplication($config);
