579bf1d6ca1caf7032cbb5c6f98c19b6bce40f84
Christoffer Niska <ChristofferNiska@gmail.com>
2013-07-24 22:13:46 +0300

    Add bootstrapped codeception and remove phpunit test

--- app/tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config = __DIR__ . '/../config/test.php';
-
-require(__DIR__ . '/../../vendor/yiisoft/yii/framework/yiit.php');
-require(__DIR__ . '/WebTestCase.php');
-
-Yii::createWebApplication($config);
