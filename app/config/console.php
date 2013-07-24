7bc1b53fbae66917e2c2167112eb8a469bbd7b2a
Christoffer Niska <ChristofferNiska@gmail.com>
2013-07-24 22:22:34 +0300

    Add permissions to the codeception log folder and update .gitignore

--- app/config/console.php
+++ app/config/console.php
@@ -21,6 +21,7 @@ return array(
             'basePath' => __DIR__ . '/../../',
             'permissions' => array(
                 'app/runtime' => array('mode' => 0777),
+                'app/tests/_log' => array('mode' => 0777),
                 'app/yiic' => array('mode' => 0755),
                 'web/assets' => array('mode' => 0777),
             ),
