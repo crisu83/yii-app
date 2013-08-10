4fc26ac1b9b7a54d4a926573094647909c7c4dce
Christoffer Niska <ChristofferNiska@gmail.com>
2013-08-10 14:42:53 +0300

    Update configurations for Vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,26 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+# Vagrantfile API/syntax version
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  # Define the box name
   config.vm.box = "precise32"
+
+  # Define the box url in case Vagrant needs to download it
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
-  config.vm.provision :shell, :path => "bootstrap.sh"
+
+  # Enable port forwarding
   config.vm.network :forwarded_port, host: 4567, guest: 80
+
+  # Enable the bootstrap-script
+  config.vm.provision :shell, :path => "bootstrap.sh"
+
+  # Change virtualbox to allow for symlinks inside the vagrant folder
+  config.vm.provider "virtualbox" do |v|
+    v.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/vagrant", "1"]
+  end
+
 end
---
+++ bootstrap.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Install Apache
+apt-get update
+apt-get install -y apache2 libapache2-mod-rewrite libapache2-mod-php5
+rm -rf /var/www
+ln -fs /vagrant /var/www
+a2enmod rewrite
+
+# Install PHP
+apt-get install -y php5 php5-curl php5-gd php5-mcrypt php5-mysql
+
+# Install MySQL
+echo mysql-server mysql-server/root_password select "vagrant" | debconf-set-selections
+echo mysql-server mysql-server/root_password_again select "vagrant" | debconf-set-selections
+apt-get install -y mysql-server-5.5 php5-mysql
+mysql -u root -p vagrant -e "CREATE DATABASE yii_app;"
+
+# Install node.js
+apt-get install -y python-software-properties python g++ make
+add-apt-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs
+
+# Install Grunt CLI globally and required node modules
+npm install -g grunt-cli
+cd /vagrant
+npm install
+
+# Install screen
+apt-get install -y screen
+
+# Apply Yii database migrations
+php /vagrant/app/yiic migrate --interactive=false
+
+# Restart Apache
+service apache2 restart
